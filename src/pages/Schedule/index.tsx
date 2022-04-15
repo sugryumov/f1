6fc689cc83b9b981dc0c425de55a738d5947ac4b
sugryumov <snugryumov@gmail.com>
2022-04-15 09:03:45 +0300

    add base styles in schedule page

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { Layout, Result } from 'antd';
 import { useGetScheduleQuery } from '@/services/scheduleService';
+import { Title } from '@/components/Title';
+import './index.css';
 
 export const Schedule: FC = () => {
   const { data, error, isFetching } = useGetScheduleQuery({});
@@ -11,6 +13,8 @@ export const Schedule: FC = () => {
     <Layout.Content className="container">
       <div className="schedule__container">
         {error && <Result status="error">{error}</Result>}
+
+        <Title loading={isFetching} text="F1 Schedule 2022" />
       </div>
     </Layout.Content>
   );
