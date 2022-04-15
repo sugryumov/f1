7e87e2c7c8b31075ee5ee746816df5c6e0fe4360
sugryumov <snugryumov@gmail.com>
2022-04-15 09:26:20 +0300

    add schedule page description

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -19,3 +19,14 @@ export const Schedule: FC = () => {
     </Layout.Content>
   );
 };
+
+// Schedule page
+// Get current Schedule http://ergast.com/api/f1/current
+
+// On click open page with results
+// Race result http://ergast.com/api/f1/2022/1/driverStandings
+// or http://ergast.com/api/f1/2022/1/results
+
+// Qualifications result http://ergast.com/api/f1/{{year}}/{{round}}/qualifying
+
+// Pit stops http://ergast.com/api/f1/{{year}}/{{round}}/pitstops
