72e7c82f774793596ce45fd141c563727449f22c
sugryumov <snugryumov@gmail.com>
2022-04-18 16:14:24 +0300

    schedule styles: create grid

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,13 +1,28 @@
 import { FC } from 'react';
 import { Layout, Result } from 'antd';
 import { useGetScheduleQuery } from '@/services/scheduleService';
+import { RaceStatuses } from '@/enums/raceStatuses';
 import { Title } from '@/components/Title';
 import './index.css';
 
 export const Schedule: FC = () => {
   const { data, error, isFetching } = useGetScheduleQuery({});
 
-  console.log('data', data);
+  const renderContent = () =>
+    data?.map(({ key, status, RaceName }, idx: number) => {
+      let styled = 'schedule__item';
+
+      if (status === RaceStatuses.NEXT) {
+        styled += idx % 3 === 1 ? ' schedule__item-8' : ' schedule__item-12';
+      }
+
+      return (
+        <div key={key} className={styled}>
+          <p>{RaceName}</p>
+          <p>{status}</p>
+        </div>
+      );
+    });
 
   return (
     <Layout.Content className="container">
@@ -15,6 +30,8 @@ export const Schedule: FC = () => {
         {error && <Result status="error">{error}</Result>}
 
         <Title loading={isFetching} text="F1 Schedule 2022" />
+
+        <div className="schedule__list">{renderContent()}</div>
       </div>
     </Layout.Content>
   );
