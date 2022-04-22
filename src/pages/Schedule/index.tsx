1654482a6ba17982a35a2b698a42a46bc3353d63
sugryumov <snugryumov@gmail.com>
2022-04-22 15:45:52 +0300

    schedule page markup

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -3,23 +3,55 @@ import { Layout, Result } from 'antd';
 import { useGetScheduleQuery } from '@/services/scheduleService';
 import { RaceStatuses } from '@/enums/raceStatuses';
 import { Title } from '@/components/Title';
+import { MONTH } from '@/common/constants/month';
+import SVGIcon from '@/common/SVGIcon';
 import './index.css';
 
 export const Schedule: FC = () => {
   const { data, error, isFetching } = useGetScheduleQuery({});
 
   const renderContent = () =>
-    data?.map(({ key, status, RaceName }, idx: number) => {
+    data?.map((race, idx) => {
+      const { key, round, status, Date: dateRace, FirstPractice } = race;
+
       let styled = 'schedule__item';
 
       if (status === RaceStatuses.NEXT) {
         styled += idx % 3 === 1 ? ' schedule__item-8' : ' schedule__item-12';
       }
 
+      const [, startMonth, startDate] = FirstPractice.Date.split('-');
+      const [, endMonth, endDate] = dateRace.split('-');
+
+      const monthText =
+        startMonth === endMonth
+          ? MONTH[endMonth]
+          : `${MONTH[startMonth]}-${MONTH[endMonth]}`;
+
       return (
         <div key={key} className={styled}>
-          <p>{RaceName}</p>
-          <p>{status}</p>
+          <span className="schedule__item-round">{`ROUND ${round}`}</span>
+          <div className="schedule__item-header">
+            <div className="schedule__item-header-date">
+              <p className="schedule__item-header-days">
+                {`${startDate} - ${endDate}`}
+              </p>
+
+              <p className="schedule__item-header-month">{monthText}</p>
+            </div>
+
+            <div className="schedule__item-info-flag">
+              <SVGIcon name={key} />
+            </div>
+
+            {/* <p className="schedule__item-header-pos">{position}</p>
+            <div className="schedule__item-header-wrap">
+              <p className="schedule__item-header-pts">{points}</p>
+              <p className="schedule__item-header-meta">PTS</p>
+            </div> */}
+          </div>
+          {/* <p>{RaceName}</p>
+          <p>{status}</p> */}
 
           <div className="schedule__item-inner">
             <img
